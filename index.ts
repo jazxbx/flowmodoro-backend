49f9c0212182065a3e69d24cda111066a8348af0
jazz <jazxbx@gmail.com>
2024-07-18 20:31:09 -0700

    add dependencies

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-dotenv.config();
-// const prisma = new PrismaClient();
-const app = express();
-app.use(cors());
+export const prisma = new PrismaClient();
 
+const app = express();
+dotenv.config();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send({ success: true, message: 'Hello world' });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
