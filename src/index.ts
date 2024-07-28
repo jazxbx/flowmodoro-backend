8c33df1a70cd6eef3e69e5e529e09e91ae789c2d
jazxbx <jazxbx@gmail.com>
2024-07-28 14:47:47 -0700

    index set up

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+//Load env variables
+dotenv.config();
+
+// Routes
+import { authRouter, viewsRouter } from './routes';
+
+// Environment Variables Setup
+const PORT = process.env.PORT;
+
+// Initial Setup
+const app = express();
+export const prisma = new PrismaClient();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routers
+app.use('/v1', viewsRouter);
+app.use('/v1/auth', authRouter);
+// app.use('/v1/users', usersRouter);
+
+//Not Found
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
