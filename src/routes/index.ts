31d2fe0b48af6f52f953adc15f6a3a80b513d6ef
jazxbx <jazxbx@gmail.com>
2024-08-01 12:43:12 -0700

    add tasks to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export { router as authRouter } from './api/auth';
 export { router as viewsRouter } from './api/views';
+export { router as tasksRouter } from './api/tasks';
 // export { router as userRouter } from './api/users';
