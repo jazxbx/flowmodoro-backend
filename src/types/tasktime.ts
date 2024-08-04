9f0ee44791c8d11aeec5b871e234b7a03ef4141a
jazxbx <jazxbx@gmail.com>
2024-08-04 15:18:23 -0700

    remove duration in tasktime schema

--- src/types/tasktime.ts
+++ src/types/tasktime.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 export const taskTimeRequestBody = z.object({
-  taskId: z.string().uuid(),
   started: z.string().datetime(),
-  ended: z.string().datetime(),
-  duration: z.number(),
+  ended: z.string().datetime().optional().optional(),
+  // duration: z.number(),
+  taskId: z.string().uuid(),
 });
 
 export type taskTimeRequestBody = z.infer<typeof taskTimeRequestBody>;
