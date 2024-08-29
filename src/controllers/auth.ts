c793432991efc1ec6349f5c0d399b6dcf4b14801
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-08-28 21:18:12 -0700

    add user registration and login functionality

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,80 +1,85 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express';
+
 import { prisma } from '../index';
+import { userRequestBody } from '../types/users';
 
-type UserType = {
-  email: string;
-  password: string;
-};
-
-//TODO: add Oauth, cookies, logout
+//TODO: add Oauth, cookies, logout, cookie parser, safeparse from zod?
+//TODO: store jwt in cookie, study how to use middleware
 
 // REGISTER USER POST REQ route: api/v1/auth/register
 
 const users = express.Router();
 
-export const register = users.post('/register', async (req, res) => {
-  try {
-    const { email, password }: UserType = req.body;
-    const trimmedEmail = email.trim();
-    const trimmedPassword = password.trim();
+export const register = users.post(
+  '/register',
+  async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body as userRequestBody;
 
-    if (!trimmedEmail || !trimmedPassword) {
-      return res.send({
-        success: false,
-        error: 'Email / Password Required',
+      if (!email || !password) {
+        return res.send({
+          success: false,
+          error: 'Email / Password Required',
+        });
+      }
+
+      const checkUser = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
       });
+
+      if (checkUser) {
+        return res.send({
+          sucess: false,
+          error: 'This email is already in use, please login',
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      const expiresIn = 60 * 60 * 24 * 7; // 7 days
+
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn,
+      });
+
+      res.send({
+        success: true,
+        message: 'User successfully created',
+        token,
+      });
+    } catch (e) {
+      console.log('error registering user', e);
+      if (
+        typeof e === 'object' &&
+        e &&
+        'message' in e &&
+        typeof e.message === 'string'
+      )
+        return res.send({
+          success: false,
+          error: e.message,
+        });
     }
-
-    const checkUser = await prisma.user.findUnique({
-      where: {
-        email: trimmedEmail,
-      },
-    });
-
-    if (checkUser) {
-      return res.send({
-        sucess: false,
-        error: 'This email is already in use, please login',
-      });
-    }
-
-    const hashedPassword = await bcrypt.hash(trimmedPassword, 10);
-
-    const user = await prisma.user.create({
-      data: {
-        email: trimmedEmail,
-        password: hashedPassword,
-      },
-    });
-
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
-    res.send({
-      success: true,
-      message: 'user successfully created',
-      token,
-    });
-  } catch (e) {
-    console.log('error registering user', e);
-    if (
-      typeof e === 'object' &&
-      e &&
-      'message' in e &&
-      typeof e.message === 'string'
-    )
-      return res.send({
-        success: false,
-        error: e.message,
-      });
   }
-});
+);
 
 // LOGIN USER POST REQ route: api/v1/auth/login
 
 export const login = users.post('/login', async (req, res) => {
   try {
-    const { email, password }: UserType = req.body;
+    const { email, password } = req.body as userRequestBody;
     if (!email || !password) {
       return res.send({
         success: false,
@@ -99,10 +104,15 @@ export const login = users.post('/login', async (req, res) => {
         error: 'User and/or password is invalid.',
       });
     }
-    const token = jwt.sign({ userId: user?.id }, process.env.JWT_SECRET);
+
+    const expiresIn = 60 * 60 * 24 * 7; // 7 days
+
+    const token = jwt.sign({ userId: user?.id }, process.env.JWT_SECRET, {
+      expiresIn,
+    });
     res.send({
       success: true,
-      message: 'user logged in',
+      message: 'User successfully logged in',
       token,
     });
   } catch (e) {
@@ -119,3 +129,5 @@ export const login = users.post('/login', async (req, res) => {
       });
   }
 });
+
+//TODO: logout route?
